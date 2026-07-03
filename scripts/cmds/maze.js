2e5a4f20a61af43663c69e2adfe9e9d1882019e2
Ismael03-Dev <backupff19@gmail.com>
2026-07-03 10:29:18 +0200

    Update maze.js

--- scripts/cmds/maze.js
+++ scripts/cmds/maze.js
@@ -4,7 +4,7 @@ const { createCanvas } = require("canvas");
 const axios = require("axios");
 
 const FORMAT_URL = "https://numbers-conversion.vercel.app/api/format";
-const CASH_URL = "https://cash-api-five.vercel.app/api/cash";
+const CASH_URL = "https://money-user-two.vercel.app/api/cash";
 
 const MAX_LIMIT = 10n ** 261n;
 const STATS_FILE = path.join(__dirname, "labyrinthe_stats.json");
